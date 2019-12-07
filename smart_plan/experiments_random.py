a51fb49200dcc962ba5d1bb03d21edf58abcd46f
Julien Romero <romerojulien34@gmail.com>
2019-12-07 19:25:41 +0100

    add fake susie

--- smart_plan/experiments_random.py
+++ smart_plan/experiments_random.py
@@ -1,5 +1,11 @@
 from smart_plan.function import Function
 from smart_plan import utils
+from sys import argv
+
+if len(argv) > 1:
+    filename = argv[1]
+else:
+    filename = "result_experiments_random_new.tsv"
 
 
 def get_random_relations(n):
@@ -47,6 +53,7 @@ if __name__ == '__main__':
 
             answered_us = 0
             answered_susie = 0
+            answered_fake_susie = 0
 
             for relation in relations:
                 query = Function()
@@ -64,16 +71,15 @@ if __name__ == '__main__':
                     answered_us += 1
                 susie_res, paths = utils.do_susie(linear_paths, relation)
                 if not res_us and susie_res:
-                    print("Problem here")
-                    print(paths)
-                    exit(0)
-                if susie_res:
+                    answered_fake_susie += 1
+                elif susie_res:
                     answered_susie += 1
 
             print(answered_us / len(relations) * 100.0, "% queries were answered for us")
             print(answered_susie / len(relations) * 100.0, "% queries were answered for susie")
-            with open("result_experiments_random_new.tsv", "a") as f:
+            print(answered_fake_susie / len(relations) * 100.0, "% queries were wrongly answered for susie")
+            with open(filename, "a") as f:
                 f.write("\t".join(map(str, [min_length, max_length, n_relation,
-                    n_function, proba_existential, 0, answered_us / len(relations)])) +  "\n")
+                    n_function, proba_existential, 0, answered_us / len(relations), 0])) + "\n")
                 f.write("\t".join(map(str, [min_length, max_length, n_relation,
-                    n_function, proba_existential, 1, answered_susie / len(relations)])) +  "\n")
+                    n_function, proba_existential, 1, answered_susie / len(relations), answered_fake_susie / len(relations)])) +  "\n")
