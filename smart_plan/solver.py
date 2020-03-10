9e290491e5830cbee3ea3f7297aaaaf4cb127e37
Julien Romero <romerojulien34@gmail.com>
2020-03-10 17:27:57 +0100

    add max length in solver

--- smart_plan/solver.py
+++ smart_plan/solver.py
@@ -15,7 +15,7 @@ class Solver:
             self.relations = sorted(utils.get_all_relations(functions))
         self.fst = utils.get_transducer_parser(functions)
 
-    def solve(self, query_relation):
+    def solve(self, query_relation, max_length=-1):
         if query_relation not in self.relations:
             return None
         query = Function()
@@ -24,6 +24,12 @@ class Solver:
         cfg = query.get_longest_query_grammar(self.relations, self.uids)
         cfg = cfg.intersection(deter)
         if not cfg.is_empty():
-            for word in cfg.get_words():
+            for word in cfg.get_words(max_length=max_length):
                 return utils.get_translation(self.fst, word)
+            raise ExplorationNotDeepEnough("The depth " + str(max_length) +
+                                           " given was not high enough.")
         return None
+
+
+class ExplorationNotDeepEnough(Exception):
+    pass
